d66e3e8e6677d19dacb1a1016a81297cb889e7f2
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-02-14 22:42:23 +0600

    make a directory and file

--- file_reading.js
+++ file_reading.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
 
+// make dir
+fs.mkdir("views", err => {
+	if(err) throw err;
+	console.log("new folder created");
+	fs.writeFile("./views/new.txt","Hi I am a new file!", 'utf8', err=> {
+		if(err) throw err;
+		console.log("new file created");
+	})
+})
+
 // Read directory
 fs.readdir('./', (err,content) =>{
 	if(err) throw err;
